efecde3e2d15bd38f474ff1c53a94a6174896305
Tamil Selvi <tamilselvi072001@gmail.com>
2025-04-15 20:41:53 +0530

    Header added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "../src/appRoutes/AppRoutes";
+import Home from "./component/home/Home";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <AppRouter />
+      {/* <AppRouter /> */}
+      <Home />
     </BrowserRouter>
   );
 };
 
 export default App;
+// 0e1d35
---
+++ src/component/home/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.scss";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="left">FocusHub</div>
+      <div className="right">Login</div>
+    </div>
+  );
+}
+
+export default Header;
